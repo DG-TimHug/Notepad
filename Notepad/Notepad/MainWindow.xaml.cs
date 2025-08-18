4b7c2c437a72b51ec6d2248d40755f45f8a6e910
Tim <tim.hug@digitecgalaxus.ch>
2025-08-18 11:49:15 +0200

    Added Feature to open txt files

--- Notepad/Notepad/MainWindow.xaml.cs
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +20,15 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        var dialog = new Microsoft.Win32.OpenFileDialog();
+        dialog.FileName = "Document";
+        dialog.DefaultExt = ".txt";
+        dialog.Filter = "Text documents (.txt)|*.txt";
+        var result = dialog.ShowDialog();
+        if (result == true)
+        {
+            var filename = dialog.FileName;
+            tbContents.Text = File.ReadAllText(filename);
+        }
     }
 }
