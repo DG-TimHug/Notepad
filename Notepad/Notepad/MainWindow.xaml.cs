fd44f044655534b7fcd800b79a940fe64b781ff9
Tim <tim.hug@digitecgalaxus.ch>
2025-08-19 11:12:17 +0200

    fixed merge oop

--- Notepad/Notepad/MainWindow.xaml.cs
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
 ﻿using System.IO;
-using System.Reflection.Metadata;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Notepad;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow 
 {
     private string? currentFilePath;
+    
     public MainWindow()
     {
         InitializeComponent();
@@ -38,13 +29,15 @@ public partial class MainWindow : Window
             TbContents.Text = File.ReadAllText(currentFilePath);
         }
     }
-
-    private void SaveFile()
+    
+    private void SaveAsFile()
     {
-        var dialog = new Microsoft.Win32.SaveFileDialog();
-        dialog.FileName = "Document";
-        dialog.DefaultExt = ".txt";
-        dialog.Filter = "Text documents (.txt)|*.txt";
+        var dialog = new Microsoft.Win32.SaveFileDialog
+        {
+            FileName = "Document",
+            DefaultExt = ".txt",
+            Filter = "Text documents (.txt)|*.txt"
+        };
         var result = dialog.ShowDialog();
         if (result == true)
         {
@@ -52,6 +45,7 @@ public partial class MainWindow : Window
             File.WriteAllText(currentFilePath, TbContents.Text );
         }
     }
+    
     private void OnClickOpenFile(object sender, RoutedEventArgs e)
     {
         OpenFile();
