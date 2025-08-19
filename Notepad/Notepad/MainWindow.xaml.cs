904c5a3c956cda8760c1fcde5ec693d7d619e65d
Tim <tim.hug@digitecgalaxus.ch>
2025-08-19 09:50:48 +0200

    Added Menu bar
    To be added: Open/save File with button combinination

--- Notepad/Notepad/MainWindow.xaml.cs
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -1,53 +1,43 @@
 ﻿using System.IO;
-using System.Reflection.Metadata;
-using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace Notepad;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    private string? filename;
     public MainWindow()
     {
         InitializeComponent();
-        
     }
 
     private void OpenFile()
     {
-        var dialog = new Microsoft.Win32.OpenFileDialog();
-        dialog.FileName = "Document";
-        dialog.DefaultExt = ".txt";
-        dialog.Filter = "Text documents (.txt)|*.txt";
+        var dialog = new Microsoft.Win32.OpenFileDialog
+        {
+            FileName = "Document",
+            DefaultExt = ".txt",
+            Filter = "Text documents (.txt)|*.txt"
+        };
         var result = dialog.ShowDialog();
         if (result == true)
         {
-            var filename = dialog.FileName;
+            filename = dialog.FileName;
             TbContents.Text = File.ReadAllText(filename);
         }
     }
 
-    private void SaveFile()
+    private void SaveAsFile()
     {
-        var dialog = new Microsoft.Win32.SaveFileDialog();
-        dialog.FileName = "Document";
-        dialog.DefaultExt = ".txt";
-        dialog.Filter = "Text documents (.txt)|*.txt";
+        var dialog = new Microsoft.Win32.SaveFileDialog
+        {
+            FileName = "Document",
+            DefaultExt = ".txt",
+            Filter = "Text documents (.txt)|*.txt"
+        };
         var result = dialog.ShowDialog();
         if (result == true)
         {
-            var filename = dialog.FileName;
+            filename = dialog.FileName;
             File.WriteAllText(filename, TbContents.Text );
         }
     }
@@ -58,6 +48,53 @@ public partial class MainWindow : Window
 
     private void OnClickSaveAsFile(object sender, RoutedEventArgs e)
     {
-        SaveFile();
+        SaveAsFile();
+    }
+
+    private void OnClickSaveFile(object sender, RoutedEventArgs e)
+    {
+        if (filename != null)
+        {
+            File.WriteAllText(filename, TbContents.Text );
+        }
+        else
+        {
+            SaveAsFile();
+        }
+    }
+    private void Bold_Checked(object sender, RoutedEventArgs e)
+    {
+        TbContents.FontWeight = FontWeights.Bold;
+    }
+
+    private void Bold_Unchecked(object sender, RoutedEventArgs e)
+    {
+        TbContents.FontWeight = FontWeights.Normal;
+    }
+
+    private void Italic_Checked(object sender, RoutedEventArgs e)
+    {
+        TbContents.FontStyle = FontStyles.Italic;
+    }
+
+    private void Italic_Unchecked(object sender, RoutedEventArgs e)
+    {
+        TbContents.FontStyle = FontStyles.Normal;
+    }
+
+    private void IncreaseFont_Click(object sender, RoutedEventArgs e)
+    {
+        if (TbContents.FontSize < 18)
+        {
+            TbContents.FontSize += 2;
+        }
+    }
+
+    private void DecreaseFont_Click(object sender, RoutedEventArgs e)
+    {
+        if (TbContents.FontSize > 10)
+        {
+            TbContents.FontSize -= 2;
+        }
     }
 }
