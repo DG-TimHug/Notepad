2a8e14b86001936b56a40d9e39f37af112482d60
Tim <tim.hug@digitecgalaxus.ch>
2025-08-20 14:29:12 +0200

    Mulitple Tabs implmented
    working

--- Notepad/Notepad/MainWindow.xaml.cs
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Globalization;
-using System.IO;
+﻿//hi :)
 using System.Windows;
+using System.Windows.Controls;
 
 namespace Notepad;
 
@@ -14,21 +14,44 @@ public partial class MainWindow
     public MainWindow()
     {
         InitializeComponent();
+        Tabs.SelectionChanged += TabsChanged;
         UpdateFontSize();
+    }
+
+    private void TabsChanged(object sender, RoutedEventArgs e)
+    {
+        if (GetSelectedTab() == 0)
+        {
+            UpdateFontSize();
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            UpdateFontSize();
+        }
     }
 
     private void UpdateFontSize()
     {
-        FontSize.Header = GetFontSize();
-    }
-
-    public double GetFontSize()
-    {
-        return (double) MainTextBox.FontSize;
+        FontSize.Header = GetTabFontSize();
     }
     
+    public double GetTabFontSize()
+    {
+        if (GetSelectedTab() == 0)
+        {
+            return MainTextBox.FontSize;
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            return SecondaryTextbox.FontSize;
+        }
+
+        return -1;
+    }  
     public void OnClickOpenFile(object sender, RoutedEventArgs e)
-    {        
+    {
         var dialog = new Microsoft.Win32.OpenFileDialog();
         dialog.FileName = "Document";
         dialog.DefaultExt = ".txt";
@@ -37,10 +60,19 @@ public partial class MainWindow
         
         if (result == true)
         {
-            var fileUtils = new FileUtils();
-            currentFilePath = dialog.FileName;
-            MainTextBox.Text = fileUtils.OpenFile(currentFilePath);
-            SecondaryTextbox.Text = fileUtils.OpenFile(currentFilePath);
+            if (GetSelectedTab() == 0)
+            {
+                var fileUtils = new FileUtils();
+                currentFilePath = dialog.FileName;
+                MainTextBox.Text = fileUtils.OpenFile(currentFilePath);
+            }
+
+            if (GetSelectedTab() == 1)
+            { 
+                var fileUtils = new FileUtils();
+                currentFilePath = dialog.FileName;
+                SecondaryTextbox.Text = fileUtils.OpenFile(currentFilePath);
+            }
         }
     }
 
@@ -55,9 +87,19 @@ public partial class MainWindow
         var result = dialog.ShowDialog();
         if (result == true)
         {
-            currentFilePath = dialog.FileName;
-            var fileUtils = new FileUtils();
-            fileUtils.SaveFile(currentFilePath, MainTextBox.Text);
+            if (GetSelectedTab() == 0)
+            {
+                currentFilePath = dialog.FileName;
+                var fileUtils = new FileUtils();
+                fileUtils.SaveFile(currentFilePath, MainTextBox.Text);
+            }
+
+            if (GetSelectedTab() == 1)
+            {
+                currentFilePath = dialog.FileName;
+                var fileUtils = new FileUtils();
+                fileUtils.SaveFile(currentFilePath, SecondaryTextbox.Text);
+            }
         }
     }
 
@@ -68,47 +110,115 @@ public partial class MainWindow
             OnClickSaveAsFile(sender, e);
             return;
         }
-        
-        var fileUtils = new FileUtils();
-        fileUtils.SaveFile(currentFilePath, MainTextBox.Text);
+        if (GetSelectedTab() == 0)
+        {
+            var fileUtils = new FileUtils();
+            fileUtils.SaveFile(currentFilePath, MainTextBox.Text);
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            var fileUtils = new FileUtils();
+            fileUtils.SaveFile(currentFilePath, SecondaryTextbox.Text);
+        }
     }
     private void Bold_Checked(object sender, RoutedEventArgs e)
     {
-        MainTextBox.FontWeight = FontWeights.Bold;
+        if (GetSelectedTab() == 0)
+        {
+            MainTextBox.FontWeight = FontWeights.Bold;
+        }
+        if (GetSelectedTab() == 1)
+        {
+            SecondaryTextbox.FontWeight = FontWeights.Bold;
+        }
     }
 
     private void Bold_Unchecked(object sender, RoutedEventArgs e)
     {
-        MainTextBox.FontWeight = FontWeights.Normal;
+        if (GetSelectedTab() == 0)
+        {
+            MainTextBox.FontWeight = FontWeights.Normal;
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            SecondaryTextbox.FontWeight = FontWeights.Normal;
+        }
     }
 
     private void Italic_Checked(object sender, RoutedEventArgs e)
     {
-        MainTextBox.FontStyle = FontStyles.Italic;
+        if (GetSelectedTab() == 0)
+        {
+            MainTextBox.FontStyle = FontStyles.Italic;
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            SecondaryTextbox.FontStyle = FontStyles.Italic;
+        }
     }
 
     private void Italic_Unchecked(object sender, RoutedEventArgs e)
     {
-        MainTextBox.FontStyle = FontStyles.Normal;
+        if (GetSelectedTab() == 0)
+        {
+            MainTextBox.FontStyle = FontStyles.Normal;
+        }
+
+        if (GetSelectedTab() == 1)
+        {
+            SecondaryTextbox.FontWeight = FontWeights.Normal;
+        }
     }
 
     private void IncreaseFont_Click(object sender, RoutedEventArgs e)
     {
-        if (MainTextBox.FontSize < 18)
+        if (GetSelectedTab() == 0)
         {
-            MainTextBox.FontSize += 1;
+            if (MainTextBox.FontSize < 18)
+            {
+                MainTextBox.FontSize += 1;
+            }
+            UpdateFontSize();
         }
-        UpdateFontSize();
+
+        if (GetSelectedTab() == 1)
+        {
+            if (SecondaryTextbox.FontSize < 18)
+            {
+                SecondaryTextbox.FontSize += 1;
+            }
+            UpdateFontSize();
+        }
     }
 
     private void DecreaseFont_Click(object sender, RoutedEventArgs e)
     {
-        if (MainTextBox.FontSize > 10)
+        if (GetSelectedTab() == 0)
         {
-            MainTextBox.FontSize -= 1;
+            if (MainTextBox.FontSize > 10)
+            {
+                MainTextBox.FontSize -= 1;
+            }
+            UpdateFontSize();
         }
-        UpdateFontSize();
+
+        if (GetSelectedTab() == 1)
+        {
+            if (MainTextBox.FontSize > 10)
+            {
+                MainTextBox.FontSize -= 1;
+            }
+            UpdateFontSize();
+        }
     }
-    
-    
+
+    private int GetSelectedTab()
+    {
+        var selectedTab = Tabs.SelectedIndex;
+        return selectedTab;
+    }
+    //Tabs.items.add/ remove
 }
